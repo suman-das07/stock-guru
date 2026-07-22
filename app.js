69bd63a0876d9edc3ea1b81819d6bfe143a78062
SUMAN DAS <sumanda0708@gmail.com>
2026-07-22 23:36:59 +0530

    function of dashboard update, which contains total products, low stock value, out of stock value, total inventory value which is calculated based on every items price * quantity. Next the Search bar/ field, where user enters the value to be searched inside the array using .filter, the value is traversed and revert in renderTable() function displaying the searched product details. the table data is rendered from LocalStorage using .innerHTML and hardcoded HTML tags in .js, with the status badge displaying [Out of Stock, Low Stock, In- Stock] based on every items quantity with a specified base value to each condition. the delete function works on the concept of .slice which is connected to item index value starts from 1. the category works same on .filter concept and value .includes(), which then being rendered in the table view. The sort is implemented using switch case and then rendered in table view UI.

--- app.js
+++ app.js
@@ -63,3 +63,182 @@ function validateForm(productName, productCategory, productPrice, productQuantit
   }
 
 }
+
+
+
+
+
+
+
+
+
+function updateDashboardCards() {
+  let productsArray = JSON.parse(localStorage.getItem(`myProducts`)) || [];
+
+  totalProdVal.textContent = `${productsArray.length}`;
+  let lowStock = 0;
+  let outOfStock = 0;
+
+  for (let val of productsArray) {
+    if (val.quantity < 10) {
+      lowStock++;
+    }
+    if (val.quantity === 0) {
+      outOfStock++;
+    }
+  }
+  let totalInventoryValue = productsArray.reduce((sum, allItems) => {
+    return sum + (allItems.price * allItems.quantity);
+  }, 0);
+
+  lowStockVal.textContent = `${lowStock}`;
+  outStockVal.textContent = `${outOfStock}`;
+  totalVal.textContent = new Intl.NumberFormat("en", { notation: "compact" }).format(totalInventoryValue);
+
+}
+if (totalProdVal) {
+  updateDashboardCards();
+}
+document.addEventListener("DOMContentLoaded", updateDashboardCards());
+
+function getStatusBadge(quantity) {
+  const qty = Number(quantity);
+  if (qty === 0) {
+    return `<span class="status out-of-stock">Out of Stock</span>`;
+  } else if (qty < 10) {
+    return `<span class="status low-stock">Low Stock</span>`;
+  } else {
+    return `<span class="status in-stock">In Stock</span>`;
+  }
+}
+
+const fullTable = document.getElementById("productTableBody");
+
+function renderTable(data) {
+  let productsArray = data || JSON.parse(localStorage.getItem(`myProducts`)) || [];
+
+  if (!fullTable) return;
+
+  fullTable.innerHTML = "";
+
+  if (productsArray.length === 0) {
+    fullTable.innerHTML = `<tr><td colspan="7" style="text-align: center;">No products found</td></tr>`;
+    return;
+  }
+
+  const tableData = productsArray.map((products, index) => {
+    return ` <tr>
+              <td>${index + 1}</td>
+              <td>${products.item}</td>
+              <td>${products.category}</td>
+              <td>${Number(products.price).toLocaleString("en-IN")}</td>
+              <td>${Number(products.quantity).toLocaleString("en-IN")}</td>
+              <td>${getStatusBadge(products.quantity)}</td>
+              <td>
+              <button class="delete-btn" onclick="deleteProduct(${index})"><i class="ri-delete-bin-6-fill"></i></button>
+              </td>
+              </tr>`;
+  }).join("");
+  fullTable.innerHTML = tableData;
+}
+document.addEventListener("DOMContentLoaded", renderTable());
+
+function deleteProduct(index) {
+  let productsArray = JSON.parse(localStorage.getItem(`myProducts`)) || [];
+
+  productsArray.splice(index, 1);
+
+  localStorage.setItem("myProducts", JSON.stringify(productsArray));
+
+  renderTable();
+  updateDashboardCards();
+}
+
+if (fullTable) {
+  renderTable();
+  updateDashboardCards();
+}
+
+const searchTag = document.getElementById("searchBar");
+
+searchTag.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    searchItem();
+  }
+})
+
+function searchItem() {
+  let productsArray = JSON.parse(localStorage.getItem(`myProducts`)) || [];
+
+  let searchedVal = searchTag.value;
+
+  let searchedItem = productsArray.filter(item => item.item.toLowerCase().includes(searchedVal));
+
+  renderTable(searchedItem);
+
+}
+
+function filterCategory() {
+  let productsArray = JSON.parse(localStorage.getItem(`myProducts`)) || [];
+
+  let categoryVal = document.getElementById("categories");
+
+  let categorySelected = categoryVal.value;
+
+  if (categorySelected === "All") {
+    renderTable();
+    return;
+  }
+
+  let categoryItem = productsArray.filter(product => product.category.includes(categorySelected));
+
+  renderTable(categoryItem);
+}
+
+function sortProducts() {
+  let productsArray = JSON.parse(localStorage.getItem(`myProducts`)) || [];
+
+  const sortVal = document.querySelector("#sort");
+
+  let copyArrayItems = [...productsArray];
+
+  switch (sortVal.value) {
+    case "Name (A–Z)":
+      copyArrayItems.sort((low, high) =>
+        low.item.localeCompare(high.item));
+      break;
+    case "Name (Z–A)":
+      copyArrayItems.sort((low, high) =>
+        high.item.localeCompare(low.item));
+      break;
+    case "Price (Low → High)":
+      copyArrayItems.sort((low, high) =>
+        low.price - high.price);
+      break;
+    case "Price (High → Low)":
+      copyArrayItems.sort((low, high) =>
+        high.price - low.price);
+      break;
+    case "Quantity (Low → High)":
+      copyArrayItems.sort((low, high) =>
+        low.quantity - high.quantity);
+      break;
+    case "Quantity (High → Low)":
+      copyArrayItems.sort((low, high) =>
+        high.quantity - low.quantity);
+      break;
+    case "Newest First":
+      copyArrayItems.sort((low, high) =>
+        high.id - low.id);
+      break;
+    case "Oldest First":
+      copyArrayItems.sort((low, high) =>
+        low.id - high.id);
+      break;
+
+    default:
+      // renderTable(productsArray)
+      break;
+  }
+  renderTable(copyArrayItems)
+}
