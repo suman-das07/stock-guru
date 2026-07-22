4df1ae92483d768365d77586f3275a4d852fddac
SUMAN DAS <sumanda0708@gmail.com>
2026-07-22 22:49:20 +0530

    targetted all html form elements and required dashboard UI values.

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ let id = 0;
 if (productsArray.length > 0) {
   id = productsArray[productsArray.length - 1].id;
 }
+
+let pName = document.querySelector("#productName");
+let pCategory = document.querySelector("#category");
+let pPrice = document.querySelector("#price");
+let pQuantity = document.querySelector("#quantity");
+
+let form = document.getElementById("productForm");
+let submitBtn = document.querySelector("#submit");
+let resetBtn = document.querySelector("#resetBtn");
+
+let totalProdVal = document.querySelector("#total-product-value");
+let lowStockVal = document.querySelector("#low-stock-value");
+let outStockVal = document.querySelector("#out-of-stock-value");
+let totalVal = document.querySelector("#total-value")
