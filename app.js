d0b164317bb473c2c0647bb8fb8e88b6160b22b0
SUMAN DAS <sumanda0708@gmail.com>
2026-07-22 22:44:18 +0530

    implemented products ID, which will initially increase and store also if the complete website is refreshed. Works on the basis of productsArray array of objects length-1

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ function saveProducts() {
   localStorage.setItem("myProducts", JSON.stringify(productsArray));
 }
 
+let id = 0;
+if (productsArray.length > 0) {
+  id = productsArray[productsArray.length - 1].id;
+}
