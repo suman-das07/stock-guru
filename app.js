53adb050b7fe7385f78806f185a8521239181314
SUMAN DAS <sumanda0708@gmail.com>
2026-07-22 23:25:32 +0530

    submit button event listener added. which contains productObject. which is then pushed to productsArray.

--- app.js
+++ app.js
@@ -22,3 +22,32 @@ let totalProdVal = document.querySelector("#total-product-value");
 let lowStockVal = document.querySelector("#low-stock-value");
 let outStockVal = document.querySelector("#out-of-stock-value");
 let totalVal = document.querySelector("#total-value")
+
+
+if (submitBtn) {
+  submitBtn.addEventListener("click", function (process) {
+    process.preventDefault();
+
+    let productName = pName.value;
+    let productCategory = pCategory.value;
+    let productPrice = Number(pPrice.value);
+    let productQuantity = Number(pQuantity.value);
+
+    if (!validateForm(productName, productCategory, productPrice, productQuantity)) {
+      return;
+    }
+    const productObject = {
+      id: ++id,
+      item: productName,
+      category: productCategory,
+      price: productPrice,
+      quantity: productQuantity,
+    }
+
+    productsArray.push(productObject);
+
+    saveProducts();
+
+    form.reset();
+  });
+}
